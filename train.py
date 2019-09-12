b24236eeb8e353154c43a35fb39fc704b89b8cbf
Yang Bian <ybian2@ncsu.edu>
2019-09-12 11:33:27 -0500

    add random_state in train.py

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-    random_state=44
+    random_state=int(sys.argv[3]) if len(sys.argv) > 3 else 44
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
